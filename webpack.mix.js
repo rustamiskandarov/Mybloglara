e0ea46871a5bffecd5bad558835ea93634eb0345
Rust <rustpo4ta@mail.ru>
2018-06-20 18:10:04 +0300

    Add assets for admin templates

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,19 @@ let mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.styles([
+    'resources/assets/bootstrap/css/bootstrap.min.css',
+    'resources/assets/font-awesome/4.5.0/css/font-awesome.min.css',
+    'resources/assets/ionicons/2.0.1/css/ionicons.min.css',
+    'resources/assets/dist/css/AdminLTE.min.css',
+    'resources/assets/dist/css/skins/_all-skins.min.css'
+], 'public/css/admin.css');
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.scripts([
+    'resources/assets/plugins/jQuery/jquery-2.2.3.min.js',
+    'resources/assets/bootstrap/js/bootstrap.min.js',
+    'resources/assets/plugins/slimScroll/jquery.slimscroll.min.js',
+    'resources/assets/plugins/fastclick/fastclick.js',
+    'resources/assets/dist/js/app.min.js',
+    'resources/assets/dist/js/demo.js'
+], 'public/js/admin.js');
